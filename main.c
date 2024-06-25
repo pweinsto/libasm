197ebcedbbf0ff683d6d660f2c26bddabc1e1e59
pweinsto <37242263+pweinstock@users.noreply.github.com>
2024-06-25 18:02:32 +0200

    add ft_write

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+
 void	HelloWorld(void);
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
 
 int	main(void)
 {
 	HelloWorld();
+	ft_write(1, "Test!\n", 6);
+	printf("return\n");
 
 	return (0);
 }
