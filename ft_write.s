867835d53cb81df9835951c6dbeeca6fd37427dd
pweinsto <37242263+pweinstock@users.noreply.github.com>
2024-07-10 18:01:56 +0200

    add errno to ft_write

--- ft_write.s
+++ ft_write.s
@@ -1,7 +1,22 @@
 global	_ft_write
 SECTION .text
 
+extern ___error	;	as defined in 	
+				;	errno.h
+				;	extern int * __error(void);
+				;	#define errno (*__error())
+
 _ft_write:
 	mov		rax, 0x2000004
 	syscall
+	jc		.error  ; on BSD based OS like macOS CarryFlag (CF) is set whether an error occured  Macos -> Darwin -> BSD -> Unix
+	ret
+
+.error:
+	mov	r15, rax	; mov the error code that is in rax to r15
+	sub rsp, 8		; https://github.com/cirosantilli/x86-assembly-cheat/blob/master/x86-64/calling-convention.md
+	call ___error	; sets the adress of the external ___error function in rax (like a function pointer)
+	add rsp, 8
+	mov	[rax], r15	; [] dreferenced the address in rax to get to the ___error function and give it the error code stored in r15. ___error funtion now sets the errno variable
+	mov	rax, -1		; the c compiler always takes the value in rax to use it as the return value for the c function
 	ret
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 
 void	HelloWorld(void);
 ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
@@ -11,8 +13,17 @@ int	main(void)
 	int fd = open("read.txt", O_RDONLY);
 
 	HelloWorld();
-	printf("return write: %zd\n", ft_write(1, "Test!\n", 6));
+	printf("return write: %zd\n", ft_write(-1, "Test!\n", 6));
 	printf("return read: %zd\n", ft_read(fd, buf, 32));
 	printf("%s\n", buf);
+
+	errno = 0;
+	write(-1, "write test\n", 11);
+	printf("errno: %d\n", errno);
+	errno = 0;
+	ft_write(-1, "ft_write test\n", 14);
+	printf("errno: %d\n", errno);
+
+
 	return (0);
 }
